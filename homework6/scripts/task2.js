6a2fb6dd2bec8c85356da4ac0eadeed3670e85c2
OtarGvritishvili <otargvritishvili@gmail.com>
2023-06-15 21:17:33 +0400

    homework6
    update.

--- homework6/scripts/task2.js
+++ homework6/scripts/task2.js
@@ -2,8 +2,18 @@ function enterProposals() {
     let pickUpProposals = document.getElementById("proposal").value;
     pickUpProposals = pickUpProposals.split(". ");
 
+    console.log(pickUpProposals);
+
+
     let editProposal = pickUpProposals.map(function(proposali) {
+        if(proposali.at(0) === proposali.at(0).toLowerCase()) {
+            // return false;
+            return proposali.at(0).toUpperCase();
+        } else {
+            return true;
+        }
 
     });
+    console.log(editProposal);
     document.getElementById("result").innerHTML = editProposal.join(". ");
 }
